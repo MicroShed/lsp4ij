f7bbfd5c8cd47a4fe60d4092bf6e65a1df4d693c
Paul Gooderham <turkeyonmarblerye@gmail.com>
2023-11-17 16:01:48 -0500

    Restore the fix to issue 328.
    Signed-off-by: Paul Gooderham <turkeyonmarblerye@gmail.com>

--- src/main/java/org/microshed/lsp4ij/DocumentContentSynchronizer.java
+++ src/main/java/org/microshed/lsp4ij/DocumentContentSynchronizer.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDocumentManager;
 import org.eclipse.lsp4j.DidChangeTextDocumentParams;
@@ -27,10 +28,20 @@ import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class DocumentContentSynchronizer implements DocumentListener {
+    /**
+     * Key for accessing / storing a set of DocumentContentSynchronizers as custom user data on a Document.
+     * This set is identical to the set of DocumentContentSynchronizers that have been registered as DocumentListeners
+     * and should be kept in synch with the addition and removal of listeners. This otherwise redundant set provides
+     * clients of the Document with a method for retrieving the DocumentContentSynchronizers which cannot be accessed
+     * through the native Document API (that provides no getDocumentListeners() method).
+     */
+    public final static Key<Set<DocumentContentSynchronizer>> KEY = Key.create(DocumentContentSynchronizer.class.getName());
     private final static Logger LOGGER = LoggerFactory.getLogger(DocumentContentSynchronizer.class);
 
     private final @NotNull LanguageServerWrapper languageServerWrapper;
@@ -96,7 +107,31 @@ public class DocumentContentSynchronizer implements DocumentListener {
         if (ApplicationManager.getApplication().isUnitTestMode()) {
             sendDidChangeEvents();
         } else {
+            if(languageServerWrapper.getProject().isDisposed()) {
+                return;
+            }
             PsiDocumentManager.getInstance(languageServerWrapper.getProject()).performForCommittedDocument(event.getDocument(), this::sendDidChangeEvents);
+        }
+    }
+
+    // REVISIT: Is there a better way to force diagnostics to be computed than sending a change event when there are no changes?
+    public void documentFullRefresh(Document document) {
+        if (syncKind == TextDocumentSyncKind.None) {
+            return;
+        }
+        checkDocument(document);
+
+        final TextDocumentContentChangeEvent changeEvent = new TextDocumentContentChangeEvent();
+        changeEvent.setText(document.getText());
+        final List<TextDocumentContentChangeEvent> events = Collections.singletonList(changeEvent);
+
+        if (ApplicationManager.getApplication().isUnitTestMode()) {
+            sendDidChangeEvents(events);
+        } else {
+            if(languageServerWrapper.getProject().isDisposed()) {
+                return;
+            }
+            PsiDocumentManager.getInstance(languageServerWrapper.getProject()).performForCommittedDocument(document, () -> sendDidChangeEvents(events));
         }
     }
 
@@ -106,7 +141,10 @@ public class DocumentContentSynchronizer implements DocumentListener {
             events = new ArrayList<>(changeEvents);
             changeEvents.clear();
         }
+        sendDidChangeEvents(events);
+    }
 
+    private void sendDidChangeEvents(List<TextDocumentContentChangeEvent> events) {
         DidChangeTextDocumentParams changeParamsToSend = new DidChangeTextDocumentParams(new VersionedTextDocumentIdentifier(), events);
         changeParamsToSend.getTextDocument().setUri(fileUri.toString());
         changeParamsToSend.getTextDocument().setVersion(++version);
@@ -206,6 +244,18 @@ public class DocumentContentSynchronizer implements DocumentListener {
         VirtualFile file = FileDocumentManager.getInstance().getFile(document);
         if (file != null) {
             LOGGER.warn(header + " file=" + file);
+        }
+    }
+
+    private void checkEvent(DocumentEvent event) {
+        checkDocument(event.getDocument());
+    }
+
+    private void checkDocument(Document eventDocument) {
+        if (this.document != eventDocument) {
+            logDocument("Listener document", this.document);
+            logDocument("Event document", eventDocument);
+            throw new IllegalStateException("Synchronizer should apply to only a single document, which is the one it was instantiated for"); //$NON-NLS-1$
         }
     }
 
